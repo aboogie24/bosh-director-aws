7a82908a20292f08b7effdac3633f4d161b1e0c9
Aboogie <albrown.ctr@kr.af.mil>
2022-08-10 14:05:24 +0300

    run-deploy.sh testing login to bosh

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,7 +2,51 @@
 # 
 
 resource "aws_iam_user" "bbl" {
-  name = "bbl-user"
+  name = "bbl_user"
+  path = "/system/"
+
+  tags = { 
+    Automation = "Terraform"
+  }
 
 }
+
+resource "aws_iam_access_key" "bbl" {
+    user = aws_iam_user.bbl.name 
+}
+
+resource "aws_iam_user_policy" "bbl" {
+    name = "bbl_policy"
+    user = aws_iam_user.bbl.name
+
+    policy = jsonencode({
+    
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "ec2:*",
+                    "cloudformation:*",
+                    "elasticloadbalancing:*",
+                    "iam:*",
+                    "route53:*",
+                    "logs:*",
+                    "kms:*"
+                ],
+                "Resource": [
+                    "*"
+                ]
+            }
+        ]
+
+    })
+}
+
+output "secret" {
+    value = aws_iam_access_key.bbl.secret
+    sensitive = false 
+}
+
+
 
